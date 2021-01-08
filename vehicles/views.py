6a57214621d9d13f9f389a38bb6a7e20fe12626f
ldettorre <ldettorre21@gmail.com>
2021-01-08 02:54:52 +0000

    Added pagination for the vehicles template allowing previous, numerical page options and next tabs

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator    
 from .models import Vehicles
 
 # Create your views here.
 
 def vehicles(request):
-    vehicles = Vehicles.objects.all()
-    return render(request, 'vehicles/vehicles.html', {"vehicles":vehicles})
+    vehicles = Vehicles.objects.order_by("-price").filter(published=True)
+    paginator = Paginator(vehicles, 3)
+    page = request.GET.get('page')
+    paged_vehicles = paginator.get_page(page)
+    return render(request, 'vehicles/vehicles.html', {"vehicles":paged_vehicles})
 
 def vehicle(request, vehicle_id):
     vehicle = get_object_or_404(Vehicles, id=vehicle_id)
