5d75bacb550069c0e5c0b0ecfef45fbf8427bc83
ldettorre <ldettorre21@gmail.com>
2021-01-09 22:56:25 +0000

    Added regex to the models.py in contact app so that only phone numbers beginning with 08 followed by 8 digits can be entered. Relevant error messages and help message will be added next

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
@@ -7,4 +8,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=50)
     message = models.TextField(max_length=250)
+   # Code below works but needs an error message because failed attempts just refresh the empty page with no warning.
+    phone_regex = RegexValidator(regex=r'^(08)\d{8}$')
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     submission_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.contrib import messages
 
@@ -6,4 +6,11 @@ from django.contrib import messages
 # Create your views here.
 
 def contact_form(request):
+    if request.method =="POST":
+        form =  ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect("contact_form")
+    else:
+        form = ContactForm()
     return render(request, 'contact/contact_form.html', {'form':form})
