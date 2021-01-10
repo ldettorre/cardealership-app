0be3b89dfdcc9d04ae8d6fda1fdb7ffa4e89bef2
ldettorre <ldettorre21@gmail.com>
2021-01-10 16:18:36 +0000

    Changed the regex in contact/models.py to accept numbers only without format as the initial feature was too restrictive for irish mobile numbers only

--- contact/forms.py
+++ contact/forms.py
@@ -5,3 +5,4 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields =('name','phone_number','email','message')
+
--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.core.validators import RegexValidator
 
+
 # Create your models here.
 
 class Contact(models.Model):
@@ -9,6 +10,10 @@ class Contact(models.Model):
     email = models.EmailField(max_length=50)
     message = models.TextField(max_length=250)
    # Code below works but needs an error message because failed attempts just refresh the empty page with no warning.
-    phone_regex = RegexValidator(regex=r'^(08)\d{8}$')
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    phone_regex = RegexValidator(regex=r'^\d+$')
+    phone_number = models.CharField(validators=[phone_regex], max_length=17)
     submission_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+
+    
+        
