db00f0e1fc383e92f4045ea97bc97fe8bdc6440b
ldettorre <ldettorre21@gmail.com>
2021-01-12 00:33:58 +0000

    I added the local static and media variables to just check if the site will run via heroku before setting up seperate AWS storage

--- cardealership/settings/prod.py
+++ cardealership/settings/prod.py
@@ -6,3 +6,11 @@ SECRET_KEY = config("SECRET_KEY")
 DATABASES= {
         'default': dj_database_url.parse(config('DATABASE_URL'))
     }
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
