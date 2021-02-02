6cd2358b816b88fac04e6aff506cbdf6ef5df408
ldettorre <ldettorre21@gmail.com>
2021-02-02 03:19:20 +0000

    I created an admin action inside the contacts app which exports contact details to csv. In the export_emails function, we loop over the contacts in the apps query set then using the csv import we write each one to a new row and return a csv file.

--- contact/admin.py
+++ contact/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from .models import Contact, CarSourcing
+import csv
+from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Register your models here.
+def export_emails(modelAdmin, request, queryset):
+    response = HttpResponse(content_type='type/csv')
+    writer = csv.writer(response)
+    writer.writerow(['name','email'])
+    for contact in queryset.values_list('name','email'):
+        writer.writerow(contact)
+    response['Content-Disposition'] = 'attachment; filename="email_list.csv"'
+
+    return response
+
+
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone_number', 'email', 'submission_date')
+    actions = [export_emails]
+
 
 class CarSourcingAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone_number', 'email', 'submission_date')
