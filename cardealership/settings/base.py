c8587f1e949c3c827ef38c00a71e492638c98dbf
ldettorre <ldettorre21@gmail.com>
2021-02-09 02:01:23 +0000

    Failed to add the trailing comma at the end of the bultin previously added

--- cardealership/settings/base.py
+++ cardealership/settings/base.py
@@ -77,7 +77,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'builtins': ['django.templatetags.static']
+            'builtins': [
+                'django.templatetags.static',
+            ]
         },
     },
 ]
