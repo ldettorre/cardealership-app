21f94daf16715d993b9ed96a719290879ce7f3db
ldettorre <ldettorre21@gmail.com>
2021-02-09 19:58:47 +0000

    I removed the builtin from base.py as it didn't seem to change anything re: the static images loading. I also removed 'cardealership' from line 24 of prod.py to see if perhaps the path is incorrect.:

--- cardealership/settings/base.py
+++ cardealership/settings/base.py
@@ -77,9 +77,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'builtins': [
-                'django.templatetags.static',
-            ]
         },
     },
 ]
