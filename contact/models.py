e346d3a993d4c96a2ed87a3178675e3ff2f984b0
ldettorre <ldettorre21@gmail.com>
2021-02-23 05:18:17 +0000

    I copied the layout of vehicles.html for the car sourcing page. the car sourcing element has been changed to a contact one but the hours remains the same. Where the vehicles would go is now the car souring form along with a title and blurb. I will be looking into how to add columns to certain fields of the form as the name field does not need to be the entire with of the column where as the likes of message should.

--- contact/models.py
+++ contact/models.py
@@ -27,13 +27,13 @@ class CarSourcing(models.Model):
     phone_number = models.CharField(validators=[phone_regex], max_length=17)
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    transmission = models.CharField(max_length=100, choices = TRANSMISSION_CHOICES)
-    variant = models.CharField(max_length=100)
     year = models.IntegerField()
+    variant = models.CharField(max_length=100)
+    transmission = models.CharField(max_length=100, choices = TRANSMISSION_CHOICES)
     condition = models.CharField(max_length=100,blank=True, choices = CONDITION_CHOICES)
     fuel = models.CharField(max_length=100, choices = FUEL_CHOICES)
     additional_information = models.TextField(max_length=250)
-    gdpr_aggrement = models.BooleanField(default=True)
+    gdpr_aggrement = models.BooleanField(default=False)
     submission_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
 
     def __str__(self):
