c8e7838d4e05deb8f29eba7db60524cb7b09a1a2
ldettorre <ldettorre21@gmail.com>
2021-02-26 22:55:14 +0000

    I deleted the blog app from the site in favour of keeping the site more functionally aligned to real world examples and am now working on the responsiveness of the vehicles page

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('', views.blog, name='blog'),
-    path('<int:blog_id>', views.blog_post, name='blog_post'),
-]
--- cardealership/settings/base.py
+++ cardealership/settings/base.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'vehicles',
     'contact',
     'storages',
-    'blog',
     'ckeditor',
     'crispy_forms',
 
