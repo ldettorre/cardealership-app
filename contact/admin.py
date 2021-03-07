1ff5b7a8f7593c44d3888c1993b376849fe9f9d2
ldettorre <ldettorre21@gmail.com>
2021-03-07 02:41:31 +0000

    Instead of creating a seperate function per contact form to download the data into a csv file, I found a way to create one function that takes any of the contact forms. Using the queryset passed through and with the help of islice, the query set is looped over once and has it's keys written as a header. It then loops over each contact entry that exists and writes them as subsequent rows. This now works for all of the forms in the contact app

--- contact/admin.py
+++ contact/admin.py
@@ -3,17 +3,24 @@ from .models import Contact, CarSourcing, EmailSubscriber
 import csv
 from django.shortcuts import render
 from django.http import HttpResponse
+from itertools import islice
 
 
 # Register your models here.
 def export_emails(modelAdmin, request, queryset):
     response = HttpResponse(content_type='type/csv')
     writer = csv.writer(response)
-    writer.writerow(['name','email'])
-    for contact in queryset.values_list('name','email'):
+    headers = []
+
+    # Each iteration over querysets values returns the full list of keys. 
+    # So in order to write them once and use them as the header, we set a limit using islice.
+    for k in islice(queryset.values(),1):
+        headers += k.keys()
+    writer.writerow(headers)
+    
+    for contact in queryset.values_list():
         writer.writerow(contact)
     response['Content-Disposition'] = 'attachment; filename="email_list.csv"'
-
     return response
 
 
