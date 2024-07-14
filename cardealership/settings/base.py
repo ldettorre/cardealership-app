23c5f89a5aa52d1e00cd9ca3af6b24bec6be1bf7
ldettorre <ldettorre21@gmail.com>
2024-07-14 21:18:50 +0100

    Updated single vehicle view page to reflect carimages image field being renamed

--- cardealership/settings/base.py
+++ cardealership/settings/base.py
@@ -138,3 +138,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = config("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
 EMAIL_PORT = 587
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
